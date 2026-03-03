a77f9ff9631c76fb0b11b4cad58f8f7603e81cf2
mohammadishaq7283-jpg <mohammadishaq7283@gmail.com>
2026-03-03 20:44:27 +0500

    Update navigation.ts

--- Src/navigation.ts
+++ Src/navigation.ts
@@ -1,7 +1,8 @@
+// src/navigation.ts
 import { createSharedPathnamesNavigation } from 'next-intl/navigation';
 
-export const locales = ['en', 'ur', 'hi', 'ar', 'es', 'fr'] as const;
-export const localePrefix = 'always'; // Default
+export const locales = ['en', 'hi', 'ur'] as const;
+export const localePrefix = 'always';
 
 export const { Link, redirect, usePathname, useRouter } =
   createSharedPathnamesNavigation({ locales, localePrefix });
