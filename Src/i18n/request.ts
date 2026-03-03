2e2de92dc240fc27cc0ad7487d796d29fa1e142e
mohammadishaq7283-jpg <mohammadishaq7283@gmail.com>
2026-03-03 20:25:05 +0500

    Update request.ts

--- Src/i18n/request.ts
+++ Src/i18n/request.ts
@@ -1,12 +1,12 @@
 import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
-import { locales } from '../navigation'; // <-- Note: '../' ho gaya hai
+import { locales } from '../navigation';
 
 export default getRequestConfig(async ({ locale }) => {
   if (!locales.includes(locale as any)) notFound();
 
   return {
-    // Note: '../../' kyunki ab hum src/i18n/ folder ke andar hain
+    // Note: '../../' zaroori hai
     messages: (await import(`../../messages/${locale}.json`)).default
   };
 });
